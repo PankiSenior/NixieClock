cebbaff3157cc38b9cb8f7c1ffda3896f2d92acc
PankiSenior <klaus_pankratz@web.de>
2017-02-05 18:22:01 +0100

    first commit

---
+++ pollinBoardx.ino
@@ -0,0 +1,22 @@
+# include <avr/io.h>
+
+byte b;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+DDRD = B00111100;
+}
+
+
+void loop() {
+ // put your main code here, to run repeatedly:
+ digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+ b=4;
+ PORTD = 4;//B00000100;
+ delay(1000);                       // wait for a second
+ 
+ digitalWrite(13, LOW);             // turn the LED off by making the voltage LOW
+ PORTD = B00000000;    
+ delay(1000);                       // wait for a second
+}
